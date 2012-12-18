36d9405febe9e9d863b065461597fca434e4570b
seece <pekka.vaananen@iki.fi>
2012-12-18 17:30:50 +0200

    fix ui observer adding

--- yotris/src/yotris/logic/GameLogic.java
+++ yotris/src/yotris/logic/GameLogic.java
@@ -14,6 +14,8 @@ public class GameLogic extends Observable {
 		this.ui = ui;
 		this.settings = settings;
 		this.reset(settings);
+
+		addObserver(ui);
 	}
 
 	private void reset(Settings settings) {
