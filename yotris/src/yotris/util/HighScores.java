403a3af7ae1b22c9f72ae86809185f83f50d0de7
seece <pekka.vaananen@iki.fi>
2012-12-18 15:23:05 +0200

    add rough highscore system sketches

---
+++ yotris/src/yotris/util/HighScores.java
@@ -0,0 +1,36 @@
+package yotris.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HighScores {
+	private ArrayList<ScoreEntry> scorelist;
+	private ScoreDAO dao;
+
+	public HighScores() {
+		this.dao = new ScoreDAO();
+		this.scorelist = dao.getScorelist();
+	}
+
+	public List<ScoreEntry> getTopTen() {
+		List<ScoreEntry> topten;
+
+		Collections.sort(scorelist);	
+		topten = scorelist.subList(0, 9);
+		
+		return topten;
+	}
+
+	public boolean insertScoreEntry(String name, int score) {
+		ScoreEntry entry = new ScoreEntry(name, score);
+
+		Collections.sort(scorelist);
+		if (scorelist.get(9).getScore() < entry.getScore()) {
+			scorelist.add(entry);
+			return true;
+		}
+		
+		return false;
+	}
+}
---
+++ yotris/src/yotris/util/ScoreDAO.java
@@ -0,0 +1,24 @@
+package yotris.util;
+
+import java.util.ArrayList;
+
+
+public class ScoreDAO {
+	private String filepath;
+	private ArrayList<ScoreEntry> scorelist;
+
+	public ScoreDAO() {
+		this("data/scores.dat");
+	}	
+
+	public ScoreDAO(String filepath) {
+		 this.filepath = filepath;
+		 this.scorelist = new ArrayList<>();
+	}	
+
+	public ArrayList<ScoreEntry> getScorelist() {
+		return scorelist;
+	}
+
+
+}
