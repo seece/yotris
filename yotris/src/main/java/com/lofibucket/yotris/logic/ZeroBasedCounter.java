60ae15802ceaacac1768de4ce950f173d3c26130
seece <pekka.vaananen@iki.fi>
2012-12-20 20:12:36 +0200

    add zero based counter class

---
+++ yotris/src/main/java/com/lofibucket/yotris/logic/ZeroBasedCounter.java
@@ -0,0 +1,81 @@
+
+package com.lofibucket.yotris.logic;
+
+
+/**
+ * A simple counter class that wraps around when its value is less than zero.
+ */
+public class ZeroBasedCounter {
+	private int value;
+	private int lowerLimit;
+	private int upperLimit;
+
+	/**
+	 *	
+	 * @param value	The initial value for the counter
+	 * @param upperLimit	The limit to which the value wraps around to when
+	 * under zero
+	 */
+	public ZeroBasedCounter(int value, int upperLimit) {
+		this.value = value;
+		this.lowerLimit = 0;
+		this.upperLimit = upperLimit;
+		checkLimit();
+	}
+
+	/**
+	 *	Returns the counter value
+	 * @return
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 *
+	 * @return	counters upper limit
+	 */
+	public int getUpperLimit() {
+		return upperLimit;
+	}
+
+	/**
+	 * 
+	 * @return counters lower limit
+	 */
+	public int getLowerLimit() {
+		return lowerLimit;
+	}
+
+	/**
+	 *
+	 * @param new_value
+	 */
+	public void setValue(int new_value) {
+		this.value = new_value;	
+		checkLimit();
+	}
+
+	/**
+	 *	Decreases the counter by one, wraps around if necessary
+	 */
+	public void decrease() {
+		value--;
+		checkLimit();
+	}
+
+	/**
+	 * Tells if the counter value is zero
+	 * @return true if value is zero, otherwise false
+	 */
+	public boolean isZero() {
+		return value==0;
+	}
+
+	private void checkLimit() {
+		if (value < lowerLimit) {
+			value = upperLimit;
+		}
+	}
+
+}
