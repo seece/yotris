257975890485a2278a8ee8d2b17e0659252941ac
seece <pekka.vaananen@iki.fi>
2012-12-20 20:02:37 +0200

    add Game class documentation

--- yotris/src/main/java/com/lofibucket/yotris/logic/Game.java
+++ yotris/src/main/java/com/lofibucket/yotris/logic/Game.java
@@ -5,13 +5,23 @@ import com.lofibucket.yotris.ui.UserInterface;
 import com.lofibucket.yotris.util.Settings;
 
 
+/**
+ * The main game class that creates the ui and runs the Game class instance.
+ */
 public class Game {
 	private UserInterface ui;
 
+	/**
+	 * Game constructor
+	 * @param ui	The user interface to be used for this session
+	 */
 	public Game(UserInterface ui) {
 		this.ui = ui;
 	}
 
+	/**
+	 *	Runs the main game loop. Exits when the game ends.
+	 */
 	public void run() {
 		Settings defaultSettings = new Settings();
 		GameLogic logic = new GameLogic(ui, defaultSettings);
